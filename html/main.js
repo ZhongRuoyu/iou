b456dd0b08addd0bee452ca4923ff0639825a07b
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-08-17 14:41:02 +0800

    Initial commit

---
+++ html/main.js
@@ -0,0 +1,163 @@
+const api = "https://iou.44522361.xyz/api";
+
+async function updateUsers() {
+  const users = await fetch(`${api}/users`).then(res => res.json());
+  console.log(users);
+
+  users.forEach(user => {
+    ["username", "lender", "borrower"].forEach(id => {
+      const input = document.createElement("input");
+      input.type = id == "borrower" ? "checkbox" : "radio";
+      input.classList.add("btn-check");
+      input.name = id;
+      input.value = user.name;
+      input.id = `${id}-${user.name}`;
+      input.required = id != "borrower";
+      input.autocomplete = "off";
+      document.getElementById(id).appendChild(input);
+
+      const label = document.createElement("label");
+      label.classList.add("btn", "form-control");
+      label.htmlFor = `${id}-${user.name}`;
+      label.textContent = user.name;
+      document.getElementById(id).appendChild(label);
+    });
+  });
+}
+
+async function updateRecords() {
+  const records = await fetch(`${api}/records`).then(res => res.json());
+  console.log(records);
+
+  const table = document.getElementById("tbody-records");
+  if (table === null) {
+    return;
+  }
+
+  [...table.rows].forEach(row => row.remove());
+  Object.values(records)
+    .sort((a, b) => b.created_at - a.created_at)
+    .forEach(record => {
+      const row = table.insertRow();
+      const checkboxCell = row.insertCell();
+      const checkbox = checkboxCell.appendChild(document.createElement("input"));
+      checkbox.type = "checkbox";
+      checkbox.id = `record-checkbox-${record.id}`;
+      checkbox.setAttribute("x-record-id", record.id);
+      ["created_at", "lender", "borrower", "amount", "remarks"].forEach(key => {
+        const cell = row.insertCell();
+        switch (key) {
+          case "created_at":
+            cell.textContent = new Date(record[key]).toLocaleString();
+            break;
+          case "amount":
+            cell.textContent = `S$${(record[key] / 100).toFixed(2)}`;
+            break;
+          default:
+            cell.textContent = record[key];
+            break;
+        }
+      });
+      if (!record.active) {
+        checkbox.disabled = true;
+        row.style.textDecoration = "line-through";
+      }
+    });
+};
+
+async function updateSummary() {
+  const summary = await fetch(`${api}/summary`).then(res => res.json());
+  console.log(summary);
+
+  const table = document.getElementById("tbody-summary");
+  if (table === null) {
+    return;
+  }
+
+  [...table.rows].forEach(row => row.remove());
+  summary.forEach(item => {
+    const row = table.insertRow();
+    ["from", "to", "amount"].forEach(key => {
+      const cell = row.insertCell();
+      switch (key) {
+        case "amount":
+          cell.textContent = `S$${(item[key] / 100).toFixed(2)}`;
+          break;
+        default:
+          cell.textContent = item[key];
+          break;
+      }
+    });
+  });
+}
+
+async function addRecord() {
+  const usernameInput = document.querySelector("input[name='username']:checked");
+  if (usernameInput === null) {
+    alert("Please select a username.");
+    return;
+  }
+  const username = usernameInput.value;
+
+  const lenderInput = document.querySelector("input[name='lender']:checked");
+  if (lenderInput === null) {
+    alert("Please select a lender.");
+    return;
+  }
+  const lender = lenderInput.value;
+
+  const borrowerInputs = document.querySelectorAll("input[name='borrower']:checked");
+  if (borrowerInputs.length === 0) {
+    alert("Please select at least one borrower.");
+    return;
+  }
+  const borrowers = [...borrowerInputs.values()].map(node => node.value);
+  if (borrowers.length === 1 && borrowers.includes(lender)) {
+    alert("Lender cannot be the only borrower.");
+    return;
+  }
+
+  const amount = +document.getElementById("amount").value * 100;
+  if (isNaN(amount)) {
+    alert("Please enter a valid amount.");
+    return;
+  }
+
+  const remarks = document.getElementById("remarks").value;
+
+  const response = await fetch(`${api}/record`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ type: "BORROW", created_by: username, lender, borrowers, amount, remarks }),
+  })
+    .then(res => {
+      alert("Record added successfully.");
+      document.getElementById("form-add-record").reset();
+      usernameInput.checked = true;
+    })
+    .catch(err => alert(err));
+
+  return false;
+}
+
+(() => {
+  document.getElementById("form-add-record")
+    .addEventListener("submit", e => {
+      e.preventDefault();
+      addRecord();
+    });
+
+  Object.entries({
+    "nav-records-tab": updateRecords,
+    "nav-summary-tab": updateSummary,
+  }).forEach(([id, update]) => {
+    document.getElementById(id)
+      .addEventListener("shown.bs.tab", async () => {
+        await update();
+      });
+  });
+})();
+
+(async () => {
+  await Promise.all([updateRecords(), updateUsers()]);
+})();
---
+++ src/app.py
@@ -0,0 +1,189 @@
+import csv
+from datetime import datetime
+from itertools import combinations, permutations
+import os
+import subprocess
+from textwrap import dedent
+from collections import namedtuple
+
+from flask import Flask, request
+from flask_cors import CORS
+import sqlite3
+
+from dotenv import load_dotenv
+
+Record = namedtuple("Record", [
+    "type",
+    "lender",
+    "borrower",
+    "amount",
+    "created_by",
+    "created_at",
+    "remarks",
+])
+
+load_dotenv()
+
+DATABASE = os.getenv("DATABASE", "iou.db")
+BILLING_REPO = os.getenv("BILLING_REPO", None)
+
+
+def init():
+  with sqlite3.connect(DATABASE) as con:
+    con.cursor().execute(
+        dedent("""
+          CREATE TABLE IF NOT EXISTS Users(
+            name TEXT PRIMARY KEY
+          );
+        """)).execute(
+            dedent("""
+              CREATE TABLE IF NOT EXISTS Records(
+                id         INTEGER PRIMARY KEY AUTOINCREMENT,
+                type       TEXT NOT NULL,
+                lender     TEXT NOT NULL,
+                borrower   TEXT NOT NULL,
+                amount     INTEGER NOT NULL,
+                created_by TEXT NOT NULL,
+                created_at INTEGER NOT NULL,
+                remarks    TEXT,
+                active     BOOLEAN DEFAULT TRUE,
+                FOREIGN KEY(lender) REFERENCES Users(name),
+                FOREIGN KEY(borrower) REFERENCES Users(name),
+                FOREIGN KEY(created_by) REFERENCES Users(name),
+                CHECK(lender != borrower),
+                CHECK(amount > 0)
+              );
+            """))
+
+
+init()
+app = Flask(__name__)
+CORS(app)
+
+
+def dict_factory(cursor, row):
+  return dict(zip(list(column[0] for column in cursor.description), row))
+
+
+def ceildiv(a, b):
+  return -(a // -b)
+
+
+@app.route("/users")
+def get_users():
+  with sqlite3.connect(DATABASE) as con:
+    con.row_factory = dict_factory
+    return con.cursor().execute("SELECT * FROM Users;").fetchall()
+
+
+@app.route("/records")
+def get_records():
+  with sqlite3.connect(DATABASE) as con:
+    con.row_factory = dict_factory
+    records = con.cursor().execute("SELECT * FROM Records;").fetchall()
+    return {record["id"]: record for record in records}
+
+
+@app.route("/summary")
+def summary():
+  with sqlite3.connect(DATABASE) as con:
+    con.row_factory = dict_factory
+
+    users = con.cursor().execute("SELECT * FROM Users;").fetchall()
+    payment = {
+        pair: 0
+        for pair in permutations(list(user["name"] for user in users), 2)
+    }
+
+    records = con.cursor().execute(
+        dedent("""
+          SELECT * FROM Records
+          WHERE active = TRUE;
+        """)).fetchall()
+    for record in records:
+      pair = (record["borrower"], record["lender"])
+      payment[pair] += record["amount"]
+
+    for pair in combinations(list(user["name"] for user in users), 2):
+      reverse_pair = (pair[1], pair[0])
+      if payment[pair] < payment[reverse_pair]:
+        pair, reverse_pair = reverse_pair, pair
+      payment[pair] -= payment[reverse_pair]
+      payment.pop(reverse_pair)
+      if payment[pair] == 0:
+        payment.pop(pair)
+
+    return [{
+        "from": pair[0],
+        "to": pair[1],
+        "amount": amount
+    } for pair, amount in payment.items()]
+
+
+@app.route("/record", methods=["POST"])
+def new_record():
+  req = request.get_json()
+  for key in ["type", "lender", "borrowers", "amount", "created_by", "remarks"]:
+    if key not in req:
+      return {"success": False, "error": f"Missing field: {key}"}, 400
+
+  record_type = req["type"]
+  lender = req["lender"]
+  borrowers = req["borrowers"]
+  amount = req["amount"]
+  created_by = req["created_by"]
+  created_at = int(datetime.now().timestamp() * 1000)
+  remarks = req["remarks"]
+  amount_per_borrower = ceildiv(amount, len(borrowers))
+  records = [
+      Record(
+          record_type,
+          lender,
+          borrower,
+          amount_per_borrower,
+          created_by,
+          created_at,
+          remarks,
+      ) for borrower in borrowers if borrower != lender
+  ]
+
+  with sqlite3.connect(DATABASE) as con:
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    cur.execute("PRAGMA foreign_keys = ON;").executemany(
+        dedent("""
+          INSERT INTO Records(
+            type,
+            lender,
+            borrower,
+            amount,
+            created_by,
+            created_at,
+            remarks)
+          VALUES(?, ?, ?, ?, ?, ?, ?);
+        """), records)
+    con.commit()
+
+  if BILLING_REPO is not None:
+    subprocess.run(["git", "pull"], cwd=BILLING_REPO, check=True)
+    for record in records:
+      borrower = record.borrower
+      lender = record.lender
+      amount = record.amount / 100
+      remarks = record.remarks
+      message = f"{lender} -> {borrower}: ${amount:.2f}"
+      if remarks:
+        message += f" ({remarks})"
+      with open(
+          f"{BILLING_REPO}/records.csv", "a", encoding="utf-8",
+          newline="") as csv_file:
+        csv.writer(csv_file).writerow(record)
+      subprocess.run(["git", "add", "records.csv"],
+                     cwd=BILLING_REPO,
+                     check=True)
+      subprocess.run(["git", "commit", "-m", message],
+                     cwd=BILLING_REPO,
+                     check=True)
+    subprocess.run(["git", "push", "origin"], cwd=BILLING_REPO, check=True)
+
+  return {"success": True}
